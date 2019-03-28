9d2fb29b48a95023d6a8a8df4e54f7a0e8561a4f
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-28 17:51:58 -0400

    initial pass at shopping list app

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// building list item html
+function buildNewListItem(item) {
+  return `<li>
+    <span class="shopping-item">${item}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+}
+
+$( function() {
+  // add item
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const input = $(this).find('input');
+    const item = input.val();
+    const template = buildNewListItem(item); 
+    $('.shopping-list').prepend(template);
+    input.val('');
+  });
+
+  $('.container').keypress(function (e) {
+    if (e.which === 13) {
+      console.log('enter key hit');
+      $('form').submit();
+      return false;
+    }
+  });
+
+  // remove an item
+  $('.container').on('click', '.shopping-item-delete', function(event) {
+    const listItem = $(this).closest('li');
+    listItem.remove();
+  });
+
+  // marking item as completed
+  $('.container').on('click', '.shopping-item-toggle', function(event) {
+    const listItem = $(this).parent().siblings().first();
+    listItem.toggleClass('shopping-item__checked');
+  });
+
+});
+
